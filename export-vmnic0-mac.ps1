ebc6660eca92fc2b3fa40bca4673ea3229a80eb3
pauldiee <paul@vandieen.com>
2019-11-27 09:05:34 +0100

    vhba export added

--- export-vmnic0-mac.ps1
+++ export-vmnic0-mac.ps1
@@ -4,4 +4,8 @@ $profilename = "*ls-llz-esxi-6*"
 
 Connect-UcsCentral $ucscentral
 
+#get vnic0 mac adresses
 Get-UcsCentralServiceProfile | Get-UcsCentralVnic | Where-Object {$_.dn -like $profilename -and $_.dn -like "*vmnic0"} | Select-Object dn,addr | Export-Csv llz-mer2-vmnic0.csv
+
+#get vhba wwpn adresses
+Get-UcsCentralServiceProfile | Get-UcsCentralVhba | Where-Object {$_.dn -like $profilename -and $_.dn -like "*vhba*"} | Select-Object dn,Name,addr,Nodeaddr | Export-Csv E:\backup\exports\C1-01-Hosts-HBA-WWPN.csv
